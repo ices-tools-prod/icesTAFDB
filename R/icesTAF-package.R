73461935d8d9aaa10812fc57251f4fe747066b35
Colin Millar <colinpmillar@gmail.com>
2018-08-04 13:04:23 +0200

    package filling

---
+++ R/icesTAF-package.R
@@ -0,0 +1,19 @@
+#' @docType package
+#'
+#' @name icesTAFWeb-package
+#'
+#' @aliases icesTAFWeb
+#'
+#' @title Transparent Assessment Framework (TAF) Web Services
+#'
+#' @description
+#' R interface to access the web services of the ICES Transparent Assessment
+#' Framework (TAF) web application <http://taf.ices.dk>.
+#'
+#'
+#' @author Colin Millar.
+#'
+#' @references
+#' ICES Transparent Assessment Framework: \url{http://taf.ices.dk}.
+
+NA
