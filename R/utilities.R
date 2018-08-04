0ad34b566f7f171aefb92a99c1392240ebe09e73
Colin Millar <colinpmillar@gmail.com>
2018-08-04 13:01:55 +0200

    api utilities

---
+++ R/utilities.R
@@ -0,0 +1,66 @@
+# webservice utilities
+
+taf_uri <- function(service, ...) {
+  # set up api url
+  query <- list(...)
+  scheme <- "http"
+
+  # return url
+  httr::modify_url("",
+                   scheme = scheme,
+                   hostname = getOption("icesTAFweb.hostname"),
+                   path = service,
+                   query = if (length(query) == 0) NULL else query)
+}
+
+
+taf_webservice <- function(service, jwt = NULL, ...) {
+
+  # form uri of webservice
+  uri <- taf_uri(service, ...)
+
+  # preform request
+  taf_get(uri, jwt)
+}
+
+
+taf_get <- function(uri, jwt = NULL) {
+  if (getOption("icesTAFweb.messages"))
+    message("GETing ... ", uri)
+
+  # read url contents
+  resp <-
+    if (is.null(jwt)) {
+      httr::GET(uri)
+    } else {
+      httr::GET(uri,
+                httr::add_headers("Authorization", paste("Bearer", jwt$token )))
+    }
+
+  # return as list
+  httr::content(resp)
+}
+
+
+
+taf_get2 <- function(uri) {
+  if (getOption("icesTAF.messages"))
+    message("GETing ... ", uri)
+
+  # read url contents
+  resp <- try(httr::GET(uri))
+
+  # if this errored then there is probably no internet connection
+  if (inherits(resp, "try-error")) {
+    warning("Attempt to access webservice failed:\n", attr(resp, "condition"))
+    return(NULL)
+  } else
+  # check server is not down by inspecting response for internal server error message
+  if (httr::http_error(resp)) {
+    warning(#"Web service failure: the server is not accessible, please try again later.\n",
+            "http status message: ", httr::http_status(resp)$message, call. = FALSE)
+  }
+
+  # return as list
+  httr::content(resp)
+}
