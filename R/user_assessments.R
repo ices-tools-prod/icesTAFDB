4921e12fa7ec8285199c47013d9ff0e7cfff4f01
Colin Millar <colinpmillar@gmail.com>
2018-10-05 09:47:10 +0200

    basic get expert groups function

--- R/user_assessments.R
+++ R/user_assessments.R
@@ -2,3 +2,8 @@
 user_assessments <- function(jwt) {
   taf_webservice("user/assessments", jwt = jwt)
 }
+
+# get user expert groups
+user_expertgroups <- function(jwt) {
+  taf_webservice("user/expertgroups", jwt = jwt)
+}
